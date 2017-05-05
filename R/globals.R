d154d5c7a5246fb505a048a93ebac71e1a326ffb
Andras Sali <sali.andrew@gmail.com>
2017-05-05 20:45:54 +0200

    Set global options only if they are not set at package load-time

--- R/globals.R
+++ R/globals.R
@@ -1,9 +1,15 @@
 .onLoad <- function(libname, pkgname) {
   shiny::addResourcePath(prefix = "assets",directoryPath = system.file("assets",package="shinycssloaders"))
   shiny::addResourcePath(prefix = "css-loaders",directoryPath = system.file("css-loaders",package="shinycssloaders"))
-  options(spinner.type = 1)
+  if (is.null(getOption("spinner.type"))) {
+    options(spinner.type = 1)
+  }
   # use default size
-  options(spinner.size = 1)
+  if (is.null(getOption("spinner.size"))) {
+    options(spinner.size = 1)
+  }
   # set default colour
-  options(spinner.color="#0275D8")
+  if (is.null(getOption("spinner.color"))) {
+    options(spinner.color="#0275D8")
+  }
 }
