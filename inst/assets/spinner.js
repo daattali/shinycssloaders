55ed1ebe0ff577af7c76f4ceeb3963b8613769fa
Andras Sali <sali.andrew@gmail.com>
2017-05-16 20:09:12 +0200

    Fixes #1 by making the spinner invisible instead of using display:none

--- inst/assets/spinner.js
+++ inst/assets/spinner.js
@@ -1,6 +1,6 @@
 /* When recalculating starts, show the spinner container & hide the output */
 $(document).on('shiny:recalculating', function(event) {
-    $(".recalculating").siblings(".load-container, .shiny-spinner-placeholder").show();
+    $(".recalculating").siblings(".load-container, .shiny-spinner-placeholder").removeClass('shiny-spinner-hidden');
     $(".recalculating").siblings(".load-container").siblings('.shiny-bound-output, .shiny-output-error').css('visibility', 'hidden');
     // if there is a proxy div, hide the previous output
     $(".recalculating").siblings(".shiny-spinner-placeholder").siblings('.shiny-bound-output, .shiny-output-error').addClass('shiny-spinner-hidden');
@@ -8,7 +8,7 @@ $(document).on('shiny:recalculating', function(event) {
 
 /* When new value or error comes in, hide spinner container (if any) & show the output */
 $(document).on('shiny:value shiny:error', function(event) {
-    $("#"+event.target.id).siblings(".load-container, .shiny-spinner-placeholder").hide();
+    $("#"+event.target.id).siblings(".load-container, .shiny-spinner-placeholder").addClass('shiny-spinner-hidden');
     $("#"+event.target.id).siblings(".load-container").siblings('.shiny-bound-output').css('visibility', 'visible');
     // if there is a proxy div, show the previous output in case it was hidden
     $("#"+event.target.id).siblings(".shiny-spinner-placeholder").siblings('.shiny-bound-output').removeClass('shiny-spinner-hidden');
