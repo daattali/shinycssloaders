8ed49e125a2baa402851a942f1034f36b33d7917
Andras Sali <sali.andrew@gmail.com>
2017-12-02 08:35:07 +0100

    Introducing local state housekeeping to avoid ghosting

--- inst/assets/spinner.js
+++ inst/assets/spinner.js
@@ -1,15 +1,48 @@
-/* When recalculating starts, show the spinner container & hide the output */
-$(document).on('shiny:recalculating', function(event) {
-    $(".recalculating").siblings(".load-container, .shiny-spinner-placeholder").removeClass('shiny-spinner-hidden');
-    $(".recalculating").siblings(".load-container").siblings('.shiny-bound-output, .shiny-output-error').css('visibility', 'hidden');
+(function() {
+var output_states = [];
+
+function show_spinner(id) {
+    $("#"+id).siblings(".load-container, .shiny-spinner-placeholder").removeClass('shiny-spinner-hidden');
+    $("#"+id).siblings(".load-container").siblings('.shiny-bound-output, .shiny-output-error').css('visibility', 'hidden');
     // if there is a proxy div, hide the previous output
-    $(".recalculating").siblings(".shiny-spinner-placeholder").siblings('.shiny-bound-output, .shiny-output-error').addClass('shiny-spinner-hidden');
+    $("#"+id).siblings(".shiny-spinner-placeholder").siblings('.shiny-bound-output, .shiny-output-error').addClass('shiny-spinner-hidden');
+}
+
+function hide_spinner(id) {
+    $("#"+id).siblings(".load-container, .shiny-spinner-placeholder").addClass('shiny-spinner-hidden');
+    $("#"+id).siblings(".load-container").siblings('.shiny-bound-output').css('visibility', 'visible');
+    // if there is a proxy div, show the previous output in case it was hidden
+    $("#"+id).siblings(".shiny-spinner-placeholder").siblings('.shiny-bound-output').removeClass('shiny-spinner-hidden');
+}
+
+function update_spinner(id) {
+  if (!(id in output_states)) {
+    show_spinner(id);
+  }
+  if (output_states[id] <= 0) {
+    show_spinner(id);
+  } else {
+    hide_spinner(id);
+  }
+}
+
+$(document).on('shiny:bound', function(event){ 
+  /* if not bound before, then set the value to 0 */
+  if (!(event.target.id in output_states)) {
+    output_states[event.target.id] = 0;
+  }
+  update_spinner(event.target.id);
+});
+
+/* When recalculating starts, show the spinner container & hide the output */
+$(document).on('shiny:outputinvalidated', function(event) {
+  output_states[event.target.id] = 0;
+  update_spinner(event.target.id);
 });
 
 /* When new value or error comes in, hide spinner container (if any) & show the output */
 $(document).on('shiny:value shiny:error', function(event) {
-    $("#"+event.target.id).siblings(".load-container, .shiny-spinner-placeholder").addClass('shiny-spinner-hidden');
-    $("#"+event.target.id).siblings(".load-container").siblings('.shiny-bound-output').css('visibility', 'visible');
-    // if there is a proxy div, show the previous output in case it was hidden
-    $("#"+event.target.id).siblings(".shiny-spinner-placeholder").siblings('.shiny-bound-output').removeClass('shiny-spinner-hidden');
+  output_states[event.target.id] = 1;
+  update_spinner(event.target.id);
 });
+}());
