3e9a3ca11e0c57c34f90d410a867dbeabddcbb2a
Dean Attali <daattali@gmail.com>
2020-05-02 21:23:22 -0400

    fix bug with last commit

--- R/withSpinner.R
+++ R/withSpinner.R
@@ -15,8 +15,8 @@ withSpinner <- function(ui_element,
                         size = getOption("spinner.size", default = 1),
                         color.background = getOption("spinner.color.background"),
                         custom.css = FALSE,
-                        proxy.height,
-                        id)
+                        proxy.height = NULL,
+                        id = NULL)
 {
   stopifnot(type %in% 0:8)
   
@@ -24,19 +24,17 @@ withSpinner <- function(ui_element,
     stop("Color should be given in hex format")
   }
   
-  if (missing(proxy.height)) {
-    if (grepl("height:\\s*\\d", ui_element)) {
-      proxy.height <- NULL 
-    } else {
+  if (is.null(proxy.height)) {
+    if (!grepl("height:\\s*\\d", ui_element)) {
       proxy.height <- "400px"
     }
   }
   
   # each spinner will have a unique id, to allow seperate sizing - based on hashing the UI element code
   id <- paste0("spinner-", digest::digest(ui_element))
-
+  
   css_size_color <- shiny::tagList()
-
+  
   if (!custom.css && type != 0) {
     if (type %in% c(2, 3) && is.null(color.background)) {
       stop("For spinner types 2 & 3 you need to specify manually a background color.")
@@ -52,7 +50,7 @@ withSpinner <- function(ui_element,
       base_css <- paste(readLines(css_file), collapse = " ")
       base_css <- glue::glue(base_css, .open = "{{", .close = "}}")
     }
-
+    
     # get default font-size from css, and cut it by 25%, as for outputs we usually need something smaller
     size <- round(c(11, 11, 10, 20, 25, 90, 10, 10)[type] * size * 0.75)
     base_css <- paste(base_css, glue::glue("#{id} {{ font-size: {size}px; }}"))
@@ -61,10 +59,10 @@ withSpinner <- function(ui_element,
   
   proxy_element <- shiny::tagList()
   if (!is.null(proxy.height)) {
-    proxy_element <- shiny::div(style=glue::glue("height:{ifelse(is.null(proxy.height),'100%',proxy.height)}"),
+    proxy_element <- shiny::div(style=glue::glue("height:{proxy.height}"),
                                 class="shiny-spinner-placeholder")
   }
-  
+
   shiny::tagList(
     shiny::singleton(
       shiny::tags$head(
