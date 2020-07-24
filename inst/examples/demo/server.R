819a9c4985639602f0194eb0723907ff2404dba4
Dean Attali <daattali@gmail.com>
2020-07-24 00:55:42 -0400

    demo app: add support for custom images

--- inst/examples/demo/server.R
+++ inst/examples/demo/server.R
@@ -2,13 +2,23 @@ server <- function(input, output, session) {
   plotnum <- reactiveVal(0)
   
   output$show_example <- renderUI({
-    shinycssloaders::withSpinner(
-      plotOutput(paste0("example", plotnum())),
-      type = as.numeric(input$type),
-      color = input$col,
-      size = input$size,
-      color.background = "#fafafa"
-    )
+    if (input$type == "custom") {
+      params <- list(
+        ui_element = plotOutput(paste0("example", plotnum())),
+        image = input$image,
+        image.width = if (input$image_size_default) NULL else input$image.width,
+        image.height = if (input$image_size_default) NULL else input$image.height
+      )
+    } else {
+      params <- list(
+        ui_element = plotOutput(paste0("example", plotnum())),
+        type = as.numeric(input$type),
+        color = input$col,
+        size = input$size,
+        color.background = "#fafafa"
+      )
+    }
+    do.call(shinycssloaders::withSpinner, params)
   })
   
   observeEvent(input$update, ignoreNULL = FALSE, {
--- inst/examples/demo/ui.R
+++ inst/examples/demo/ui.R
@@ -35,7 +35,7 @@ fluidPage(
              src="github-green-right.png",
              alt="Fork me on GitHub")
   ),
-
+  
   shinydisconnect::disconnectMessage2(),
   
   div(
@@ -50,13 +50,32 @@ fluidPage(
   fluidRow(
     column(
       3,
-      selectInput("type", 
-                  div("Spinner type",
-                      helpText("Only built-in loader images are available in this demo, but you can also use custom images.")),
-                  c("0 (no spinner)" = "0", 1:8), 1),
-      colourpicker::colourInput("col", "Color", "#0275D8", showColour = "background"),
+      selectInput("type", "Spinner type",
+                  c("<Custom image>" = "custom", "0 (no spinner)" = "0", 1:8), 1),
+      conditionalPanel(
+        "input.type == 'custom'",
+        textInput(
+          "image",
+          div("Image URL", 
+              helpText("Only publicly available URLs are supported in this demo, but you can also use local images in your own apps.")
+          ),
+          "https://github.com/daattali/shinycssloaders/blob/master/inst/img/custom.gif?raw=true"
+        ),
+        checkboxInput("image_size_default", "Use original image width/height", TRUE),
+        conditionalPanel(
+          "input.image_size_default == false",
+          fluidRow(
+            column(6, numericInput("image.width", "Image width", 100)),
+            column(6, numericInput("image.height", "Image height", 100))
+          )
+        )
+      ),
+      conditionalPanel(
+        "input.type != 'custom'",
+        colourpicker::colourInput("col", "Color", "#0275D8", showColour = "background"),
+        sliderInput("size", "Size", min = 0.5, max = 5, step = 0.5, value = 1)
+      ),
       numericInput("time", "Seconds to show spinner", 1.5),
-      sliderInput("size", "Size", min = 0.5, max = 5, step = 0.5, value = 1),
       actionButton("update", "Update", class = "btn-primary btn-lg"),
     ),
     column(
