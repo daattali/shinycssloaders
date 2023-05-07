26a6d8fa96c5447fbdd1197ef23cf42b4cb4d0de
Dean Attali <daattali@gmail.com>
2023-05-07 14:17:19 -0400

    make caption text a little larger

--- R/utils.R
+++ R/utils.R
@@ -96,7 +96,7 @@ get_spinner_css_tag <- function(type, color, size, color.background, custom.css,
   }
 
   if (!is.null(caption)) {
-    base_css <- paste(base_css, glue::glue("#{id}__caption {{ color: {color}; font-size: {size}em; }}"))
+    base_css <- paste(base_css, glue::glue("#{id}__caption {{ color: {color}; font-size: {size + 0.5}em; }}"))
   }
 
   css_rules_tag <- NULL
